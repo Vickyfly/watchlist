347b6e961b41e7d799bb80a4bfcc9a027c428bcc
Vickyfly <fengtian0808@pku.edu.cn>
2022-03-11 17:05:06 +0800

    create ,edit,and delete item by form

--- app.py
+++ app.py
@@ -1,46 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 import click
+app = Flask(__name__)
 #数据库配置
 WIN=sys.platform.startswith('win')
 if WIN:
     prefix='sqlite:///'
 else:
     prefix = 'sqlite:////'
-app = Flask(__name__)
 #下面写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
 app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
 #prefix数据库文件的绝对地址
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+
 @app.cli.command() # 注册为命令
-def forge():
-    """Generate fake data."""
-    db.create_all()
-    # 全局的两个变量移动到这个函数内
-    name = 'Tian Tian'
-    movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-    ]
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
-    db.session.commit()
-    click.echo('Done.')
 @click.option('--drop', is_flag=True, help='Create after drop.')
 # 设置选项
 def initdb(drop):
@@ -52,39 +36,89 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.') # 输出提示信息
 
-@app.route('/')
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    # 全局的两个变量移动到这个函数内
+    name = 'Tian'
+    movies = [
+        {'title': '你好李焕英', 'year': '2021'},
+        {'title': '唐人街探案3', 'year': '2021'},
+        {'title': '误杀2', 'year': '2021'},
+        {'title': '长津湖', 'year': '2022'},
+        {'title': 'Mah2jong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2022'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+
+
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+@app.errorhandler(404) # 传入要处理的错误代码
+def page_not_found(e): # 接受异常对象作为参数
+    return render_template('404.html'), 404 # 返回模板和状态码
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    if request.method=='POST':
+        title=request.form.get('title')
+        year=request.form.get('year')
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
 
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
+        movie = Movie(title=title,year=year)
+        # 读取所有电影记录
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return 'User: %s' % name
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST': # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title)> 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+# 重定向回对应的编辑页面
+        movie.title = title # 更新标题
+        movie.year = year # 更新年份
+        db.session.commit() # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index')) # 重定向回主页
+    return render_template('edit.html', movie=movie) # 传入被编辑的电影记录
+@app.route('/movie/delete/<int:movie_id>', methods=['POST']) #限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id) # 获取电影记录
+    db.session.delete(movie) # 删除对应的记录
+    db.session.commit() # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index')) # 重定向回主页
 
-# @app.route('/')
-# def hello():
-#     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
-name = 'Tian Tian'
-movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
-class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(20))
-class Movie(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(60))
-    year=db.Column(db.String(4))
 
 
