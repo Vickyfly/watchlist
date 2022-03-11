abcd1a5c59806673f051b0191c2fd0bddb2ced28
Vickyfly <fengtian0808@pku.edu.cn>
2022-03-11 09:15:19 +0800

    更新

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask,render_template
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
 
+#数据库配置
+WIN=sys.platform.startswith('win')
+if WIN:
+    prefix='sqlite:///'
+else:
+    prefix = 'sqlite:////'
+app = Flask(__name__)
+#下面写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
+app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
+#prefix数据库文件的绝对地址
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
